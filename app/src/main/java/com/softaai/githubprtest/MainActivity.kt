682ec74d5ea419fe9d0b0d5f4631e0a10c13ffc7
DESKTOP-38OQRU0\Amol <amoljp19@gmail.com>
2019-02-28 00:16:06 +0530

    navigation flow created

--- app/src/main/java/com/softaai/githubprtest/MainActivity.kt
+++ app/src/main/java/com/softaai/githubprtest/MainActivity.kt
@@ -1,7 +1,9 @@
-package com.softaai.myapplication
+package com.softaai.githubprtest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +11,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onSupportNavigateUp() =
+        findNavController(nav_host_fragment).navigateUp()
+
+
 }
